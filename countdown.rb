68981bbaef6b0cb7ad9b2c9418c175701e0f0e6a
Christopher Dutcher <cadutcher@icloud.com>
2020-06-21 22:45:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+  pause = 1
+  while seconds_left > 0
+    puts "#{seconds_left} SECONDS!"
+    sleep(pause)
+    seconds_left = seconds_left - 1
+  end
+    "HAPPY NEW YEAR!"
 end
