a11af38c98ef944439fe85cad6ed28670926f9ee
Christopher Dutcher <cadutcher@icloud.com>
2020-06-21 22:51:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,21 @@
 #write your code here
 
 def countdown
-  pause = 1
   seconds_left = 12
   while seconds_left > 0
     puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+    "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep
+  pause = 1
+  seconds_left = 12
+  while seconds_left > 0
+    puts "#{seconds_left} SECONDS(S)!"
     sleep(pause)
-    seconds_left = seconds_left - 1
+    seconds_left -= 1
     "HAPPY NEW YEAR!"
   end
 end
